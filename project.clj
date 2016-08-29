0623d76d78e353d823063ba0f67548ee6aa9b5a6
Alan MacDougall <smoke@alanmacdougall.com>
2016-08-28 20:33:35 -0400

    Bump Clojure version to alpha11.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"],
+  :dependencies [[org.clojure/clojure "1.9.0-alpha11"],
                  [com.rpl/specter "0.12.0"]]
   :source-paths ["src/clj", "src/cljc"]
   :profiles
